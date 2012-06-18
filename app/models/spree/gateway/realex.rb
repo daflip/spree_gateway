35eba948f2a0d3a7368836d31a7a2e8c1d84e76e
Jerry Walsh <jerry.walsh@gmail.com>
2012-06-18 21:47:59 +0100

    test

--- app/models/spree/gateway/realex.rb
+++ app/models/spree/gateway/realex.rb
@@ -4,6 +4,8 @@ module Spree
     preference :login, :string
     preference :password, :string
     preference :account, :string, :default => "internet"
+
+    attr_accessible :preferred_login, :preferred_password
     
     def provider_class
       ActiveMerchant::Billing::RealexGateway
