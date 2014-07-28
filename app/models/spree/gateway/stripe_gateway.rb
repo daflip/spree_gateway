1b2c5190753c45162a63eeac9d798dd1e037563b
Jerry Walsh <jerry.walsh@gmail.com>
2014-07-28 19:14:12 +1200

    add support for storing creditcards with stripe

--- app/models/spree/gateway/stripe_gateway.rb
+++ app/models/spree/gateway/stripe_gateway.rb
@@ -35,6 +35,14 @@ module Spree
     def create_profile(payment)
       return unless payment.source.gateway_customer_profile_id.nil?
 
+      customer = payment.order.user
+
+      # using a stored card and there's a stored card on file?
+      if payment.source.use_stored_card? and customer.creditcard_on_file?
+        update_payment_with_profile_info(payment, customer)
+        return 
+      end
+
       options = {
         :email => payment.order.email,
         :login => preferred_login
@@ -42,68 +50,87 @@ module Spree
 
       # if we have a gateway token then assign the customer id
       # and mark the latest card as being the default
-      if payment.order.user.gateway_token.present?
-        options[:customer] = payment.order.user.gateway_token
+      if customer.gateway_token.present?
+        options[:customer] = customer.gateway_token
         options[:set_default] = true
       end
 
       response = provider.store(payment.source, options)
+      #raise payment.source.inspect
       if response.success?
-        unless options[:customer]
-          customer_token = response.params['id']
-          payment.order.user.update_attributes!({
-              gateway_token: customer_token
-            })
+        if options[:customer] 
+          gateway_token = options[:customer]
+          card_token    = response.params['id']
+        else
+          gateway_token = response.params['id']
+          card_token    = response.params['default_card']
         end
-        payment.source.update_attributes!({
-          :gateway_customer_profile_id => options[:customer] || response.params['id'],
-          :gateway_payment_profile_id => response.params['default_card']
-        })
+        customer_attrs = {
+          gateway_token: gateway_token,
+          card_token:    card_token,
+          card_details:  payment.source.attributes
+        }
+        customer.update_attributes!(customer_attrs, without_protection: true)
+        #end
+        update_payment_with_profile_info(payment, customer)
       else
         payment.send(:gateway_error, response.message)
-      end
     end
+  end
 
-    private
+  private
 
-    def options_for_purchase_or_auth(money, creditcard, gateway_options)
-      options = {}
-      options[:description] = "Order Number: #{gateway_options[:order_id]}"
-      options[:currency] = 'EUR'
+  def update_payment_with_profile_info(payment, customer)
+    payment.source.update_attributes!({
+        gateway_customer_profile_id: customer.gateway_token,
+        gateway_payment_profile_id:  customer.card_token
+      })
+  end
 
-      if customer = creditcard.payments.last.order.user.gateway_token
-        options[:customer] = customer
-        creditcard = nil
-        #raise creditcard.payments.last.order.user.inspect
-      elsif customer = creditcard.gateway_customer_profile_id
-        options[:customer] = customer
-        creditcard = nil
-      elsif token = creditcard.gateway_payment_profile_id
-        # The Stripe ActiveMerchant gateway supports passing the token directly as the creditcard parameter
-        creditcard = token
-      end
-      return money, creditcard, options
+  def options_for_purchase_or_auth(money, creditcard, gateway_options)
+    options = {}
+    options[:description] = "Order Number: #{gateway_options[:order_id]}"
+    options[:currency] = 'EUR'
+
+    customer = creditcard.payments.last.order.user
+
+    if creditcard.use_stored_card? 
+      options[:customer] = customer.gateway_token
+      creditcard = customer.card_token
+    elsif customer.gateway_token
+      options[:customer] = customer.gateway_token
+      creditcard = nil
+      #raise creditcard.payments.last.order.user.inspect
+    elsif customer = creditcard.gateway_customer_profile_id
+      options[:customer] = customer
+      creditcard = nil
+    elsif token = creditcard.gateway_payment_profile_id
+      # The Stripe ActiveMerchant gateway supports passing the token directly as the creditcard parameter
+      creditcard = token
     end
+    return money, creditcard, options
+  end
 
-    def address_for(payment)
-      {}.tap do |options|
-        if address = payment.order.bill_address
-          options.merge!(:address => {
+  def address_for(payment)
+    {}.tap do |options|
+      if address = payment.order.bill_address
+        options.merge!(:address => {
             :address1 => address.address1,
             :address2 => address.address2,
             :city => address.city,
             :zip => address.zipcode
           })
 
-          if country = address.country
-            options[:address].merge!(:country => country.name)
-          end
+        if country = address.country
+          options[:address].merge!(:country => country.name)
+        end
 
-          if state = address.state
-            options[:address].merge!(:state => state.name)
-          end
+        if state = address.state
+          options[:address].merge!(:state => state.name)
         end
       end
     end
   end
+end
+
 end
--- db/migrate/20140724222802_add_stripe_customer_token_to_spree_users.rb
+++
@@ -1,6 +0,0 @@
-class AddStripeCustomerTokenToSpreeUsers < ActiveRecord::Migration
-  def up
-    add_column :spree_users, :gateway_token, :string
-  end
-  
-end
